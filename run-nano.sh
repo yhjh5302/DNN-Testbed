321c2ace9223e6ba5c871759223e4373405ab0ee
yhjh5302 <yhjh5302@naver.com>
2021-09-11 15:46:25 +0900

    update

---
+++ run-nano.sh
@@ -0,0 +1,42 @@
+sudo docker rmi yhjh5302/alexnet-layer1-nano && \
+sudo docker rmi yhjh5302/alexnet-layer2-nano && \
+sudo docker rmi yhjh5302/alexnet-layer3-nano && \
+sudo docker rmi yhjh5302/googlenet-layer1-nano && \
+sudo docker rmi yhjh5302/googlenet-layer2-nano && \
+sudo docker rmi yhjh5302/googlenet-layer3-nano && \
+sudo docker rmi yhjh5302/mobilenet-layer1-nano && \
+sudo docker rmi yhjh5302/mobilenet-layer2-nano && \
+sudo docker rmi yhjh5302/mobilenet-layer3-nano && \
+sudo docker rmi yhjh5302/vggnet-layer1-nano && \
+sudo docker rmi yhjh5302/vggnet-layer2-nano && \
+sudo docker rmi yhjh5302/vggnet-layer3-nano && \
+sudo docker rmi yhjh5302/vggfnet-layer1-nano && \
+sudo docker rmi yhjh5302/vggfnet-layer2-nano && \
+cd AlexNet/layer1 && sudo docker build -t yhjh5302/alexnet-layer1-nano . && \
+cd ../../AlexNet/layer2 && sudo docker build -t yhjh5302/alexnet-layer2-nano . && \
+cd ../../AlexNet/layer3 && sudo docker build -t yhjh5302/alexnet-layer3-nano . && \
+cd ../../GoogLeNet/layer1 && sudo docker build -t yhjh5302/googlenet-layer1-nano . && \
+cd ../../GoogLeNet/layer2 && sudo docker build -t yhjh5302/googlenet-layer2-nano . && \
+cd ../../GoogLeNet/layer3 && sudo docker build -t yhjh5302/googlenet-layer3-nano . && \
+cd ../../MobileNet/layer1 && sudo docker build -t yhjh5302/mobilenet-layer1-nano . && \
+cd ../../MobileNet/layer2 && sudo docker build -t yhjh5302/mobilenet-layer2-nano . && \
+cd ../../MobileNet/layer3 && sudo docker build -t yhjh5302/mobilenet-layer3-nano . && \
+cd ../../VGGNet/layer1 && sudo docker build -t yhjh5302/vggnet-layer1-nano . && \
+cd ../../VGGNet/layer2 && sudo docker build -t yhjh5302/vggnet-layer2-nano . && \
+cd ../../VGGNet/layer3 && sudo docker build -t yhjh5302/vggnet-layer3-nano . && \
+cd ../../VGGFNet/layer1 && sudo docker build -t yhjh5302/vggfnet-layer1-nano . && \
+cd ../../VGGFNet/layer2 && sudo docker build -t yhjh5302/vggfnet-layer2-nano . && \
+sudo docker push yhjh5302/alexnet-layer1-nano && \
+sudo docker push yhjh5302/alexnet-layer2-nano && \
+sudo docker push yhjh5302/alexnet-layer3-nano && \
+sudo docker push yhjh5302/googlenet-layer1-nano && \
+sudo docker push yhjh5302/googlenet-layer2-nano && \
+sudo docker push yhjh5302/googlenet-layer3-nano && \
+sudo docker push yhjh5302/mobilenet-layer1-nano && \
+sudo docker push yhjh5302/mobilenet-layer2-nano && \
+sudo docker push yhjh5302/mobilenet-layer3-nano && \
+sudo docker push yhjh5302/vggnet-layer1-nano && \
+sudo docker push yhjh5302/vggnet-layer2-nano && \
+sudo docker push yhjh5302/vggnet-layer3-nano && \
+sudo docker push yhjh5302/vggfnet-layer1-nano && \
+sudo docker push yhjh5302/vggfnet-layer2-nano
---
+++ run.sh
@@ -0,0 +1,42 @@
+sudo docker rmi yhjh5302/alexnet-layer1 && \
+sudo docker rmi yhjh5302/alexnet-layer2 && \
+sudo docker rmi yhjh5302/alexnet-layer3 && \
+sudo docker rmi yhjh5302/googlenet-layer1 && \
+sudo docker rmi yhjh5302/googlenet-layer2 && \
+sudo docker rmi yhjh5302/googlenet-layer3 && \
+sudo docker rmi yhjh5302/mobilenet-layer1 && \
+sudo docker rmi yhjh5302/mobilenet-layer2 && \
+sudo docker rmi yhjh5302/mobilenet-layer3 && \
+sudo docker rmi yhjh5302/vggnet-layer1 && \
+sudo docker rmi yhjh5302/vggnet-layer2 && \
+sudo docker rmi yhjh5302/vggnet-layer3 && \
+sudo docker rmi yhjh5302/vggfnet-layer1 && \
+sudo docker rmi yhjh5302/vggfnet-layer2 && \
+cd tensorflow/AlexNet/layer1 && sudo docker build -t yhjh5302/alexnet-layer1 . && \
+cd ../../AlexNet/layer2 && sudo docker build -t yhjh5302/alexnet-layer2 . && \
+cd ../../AlexNet/layer3 && sudo docker build -t yhjh5302/alexnet-layer3 . && \
+cd ../../GoogLeNet/layer1 && sudo docker build -t yhjh5302/googlenet-layer1 . && \
+cd ../../GoogLeNet/layer2 && sudo docker build -t yhjh5302/googlenet-layer2 . && \
+cd ../../GoogLeNet/layer3 && sudo docker build -t yhjh5302/googlenet-layer3 . && \
+cd ../../MobileNet/layer1 && sudo docker build -t yhjh5302/mobilenet-layer1 . && \
+cd ../../MobileNet/layer2 && sudo docker build -t yhjh5302/mobilenet-layer2 . && \
+cd ../../MobileNet/layer3 && sudo docker build -t yhjh5302/mobilenet-layer3 . && \
+cd ../../VGGNet/layer1 && sudo docker build -t yhjh5302/vggnet-layer1 . && \
+cd ../../VGGNet/layer2 && sudo docker build -t yhjh5302/vggnet-layer2 . && \
+cd ../../VGGNet/layer3 && sudo docker build -t yhjh5302/vggnet-layer3 . && \
+cd ../../VGGFNet/layer1 && sudo docker build -t yhjh5302/vggfnet-layer1 . && \
+cd ../../VGGFNet/layer2 && sudo docker build -t yhjh5302/vggfnet-layer2 . && \
+sudo docker push yhjh5302/alexnet-layer1 && \
+sudo docker push yhjh5302/alexnet-layer2 && \
+sudo docker push yhjh5302/alexnet-layer3 && \
+sudo docker push yhjh5302/googlenet-layer1 && \
+sudo docker push yhjh5302/googlenet-layer2 && \
+sudo docker push yhjh5302/googlenet-layer3 && \
+sudo docker push yhjh5302/mobilenet-layer1 && \
+sudo docker push yhjh5302/mobilenet-layer2 && \
+sudo docker push yhjh5302/mobilenet-layer3 && \
+sudo docker push yhjh5302/vggnet-layer1 && \
+sudo docker push yhjh5302/vggnet-layer2 && \
+sudo docker push yhjh5302/vggnet-layer3 && \
+sudo docker push yhjh5302/vggfnet-layer1 && \
+sudo docker push yhjh5302/vggfnet-layer2
