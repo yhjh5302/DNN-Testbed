aa967b553fac432040412419d4f38fe086b87550
yhjh5302 <yhjh5302@naver.com>
2021-09-18 16:46:03 +0900

    update

--- tensorflow/VGGFNet/VGGFNetTestTF.py
+++ tensorflow/VGGFNet/VGGFNetTestTF.py
@@ -61,6 +61,17 @@ class VGGFNet_layer_2(keras.Model):
         return x
 
 if __name__ == '__main__':
+    set_gpu = False
+    vram_limit = 1024
+    if set_gpu:
+        gpu_devices = tf.config.list_physical_devices(device_type='GPU')
+        if not gpu_devices:
+            raise ValueError('Cannot detect physical GPU device in TF')
+        tf.config.set_logical_device_configuration(gpu_devices[0], [tf.config.LogicalDeviceConfiguration(memory_limit=vram_limit)])
+        tf.config.list_logical_devices()
+    else:
+        tf.config.set_visible_devices([], 'GPU')
+
     # load dataset
     _, (x_test, y_test) = keras.datasets.cifar10.load_data()
     x_test = x_test.reshape(10000, 32, 32, 3).astype('float32') / 255
@@ -80,13 +91,13 @@ if __name__ == '__main__':
     # for cudnn load
     layer1(x_test[0:1])
 
-    batch_size = 64
-    max = math.ceil(10000/batch_size)
+    batch_size = 1
+    max = math.ceil(1000/batch_size)
     correct, l1, l2 = 0, 0, 0
     for i in range(max):
         start = i * batch_size
         if i == max-1:
-            end = 10000
+            end = 1000
         else:
             end = (i+1) * batch_size
 
@@ -105,6 +116,6 @@ if __name__ == '__main__':
         answer = test.reshape(-1)
         correct += tf.reduce_sum(tf.cast(predict == answer, tf.float32))
 
-    print("accuracy: {:.2f}%".format(correct/100))
-    print("layer1 took {:.3f}ms".format(l1/10))
-    print("layer2 took {:.3f}ms".format(l2/10))
+    print("accuracy: {:.2f}%".format(correct/10))
+    print("layer1 took {:.3f} ms".format(l1))
+    print("layer2 took {:.3f} ms".format(l2))
