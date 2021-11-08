2fac6b61fb898675551629818faafba11f248e03
wnlab <yhjh5302@yonsei.ac.kr>
2021-11-08 15:34:01 +0900

    data generator block add

--- tensorflow/DataGenerator/DataGenerator.py
+++ tensorflow/DataGenerator/DataGenerator.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 import argparse
 #import multiprocessing as mp
 
-def image_sender(model_name, next_socket, images, labels, label_list, label_lock, time_list, time_lock, wait_time, arrival_rate, _stop_event):
+def image_sender(model_name, next_socket, images, labels, label_list, label_lock, time_list, time_lock, arrival_rate, _stop_event):
     for _ in range(1000):
         # sleep before sending
         time.sleep(1/arrival_rate)
@@ -19,9 +19,10 @@ def image_sender(model_name, next_socket, images, labels, label_list, label_lock
         with time_lock:
             time_list.append(time.time())
         send_input(next_socket, data, _stop_event)
+    #_stop_event.set()
 
 def image_recver(model_name, conn, label_list, label_lock, time_list, time_lock, _stop_event):
-    while True:
+    while _stop_event.is_set() == False:
         # make data receiving thread
         outputs = recv_output(conn, _stop_event)
         predicted = tf.argmax(outputs, 1)
@@ -57,16 +58,16 @@ if __name__ == "__main__":
     parser.add_argument('--vggfnet_prev_port', default=30040, type=int, help='Previous node port')
     parser.add_argument('--vggfnet_next_addr', default='10.96.0.241', type=str, help='Next node address')
     parser.add_argument('--vggfnet_next_port', default=30041, type=int, help='Next node port')
-    parser.add_argument('--alexnet_wait_time', default=0.500, type=float, help='waiting time for making batch')
-    parser.add_argument('--googlenet_wait_time', default=0.500, type=float, help='waiting time for making batch')
-    parser.add_argument('--mobilenet_wait_time', default=0.500, type=float, help='waiting time for making batch')
-    parser.add_argument('--vggnet_wait_time', default=0.500, type=float, help='waiting time for making batch')
-    parser.add_argument('--vggfnet_wait_time', default=0.500, type=float, help='waiting time for making batch')
-    parser.add_argument('--alexnet_arrival_rate', default=30, type=int, help='arrival rate')
-    parser.add_argument('--googlenet_arrival_rate', default=30, type=int, help='arrival rate')
-    parser.add_argument('--mobilenet_arrival_rate', default=30, type=int, help='arrival rate')
-    parser.add_argument('--vggnet_arrival_rate', default=30, type=int, help='arrival rate')
-    parser.add_argument('--vggfnet_arrival_rate', default=30, type=int, help='arrival rate')
+    parser.add_argument('--alexnet_block', default=False, type=str2bool, help='block?')
+    parser.add_argument('--googlenet_block', default=False, type=str2bool, help='block?')
+    parser.add_argument('--mobilenet_block', default=False, type=str2bool, help='block?')
+    parser.add_argument('--vggnet_block', default=False, type=str2bool, help='block?')
+    parser.add_argument('--vggfnet_block', default=False, type=str2bool, help='block?')
+    parser.add_argument('--alexnet_arrival_rate', default=1, type=int, help='arrival rate')
+    parser.add_argument('--googlenet_arrival_rate', default=1, type=int, help='arrival rate')
+    parser.add_argument('--mobilenet_arrival_rate', default=1, type=int, help='arrival rate')
+    parser.add_argument('--vggnet_arrival_rate', default=1, type=int, help='arrival rate')
+    parser.add_argument('--vggfnet_arrival_rate', default=1, type=int, help='arrival rate')
     parser.add_argument('--vram_limit', default=0, type=int, help='Next node port')
     args = parser.parse_args()
 
@@ -83,111 +84,115 @@ if __name__ == "__main__":
     images = images.reshape(10000, 32, 32, 3).astype('float32') / 255
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    alexnet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    alexnet_next_sock.settimeout(600)
-    alexnet_next_sock.connect((args.alexnet_next_addr, args.alexnet_next_port))
-    print('AlexNet next node is ready, Connected by', args.alexnet_next_addr)
+    if args.alexnet_block == False:
+        alexnet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        alexnet_next_sock.settimeout(600)
+        alexnet_next_sock.connect((args.alexnet_next_addr, args.alexnet_next_port))
+        print('AlexNet next node is ready, Connected by', args.alexnet_next_addr)
 
-    alexnet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    alexnet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    alexnet_prev_sock.bind((args.alexnet_prev_addr, args.alexnet_prev_port))
-    alexnet_prev_sock.listen()
-    alexnet_port, alexnet_addr = alexnet_prev_sock.accept()
-    print('AlexNet prev node is ready, Connected by', alexnet_addr)
+        alexnet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        alexnet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        alexnet_prev_sock.bind((args.alexnet_prev_addr, args.alexnet_prev_port))
+        alexnet_prev_sock.listen()
+        alexnet_port, alexnet_addr = alexnet_prev_sock.accept()
+        print('AlexNet prev node is ready, Connected by', alexnet_addr)
 
-    '''
-    googlenet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    googlenet_next_sock.settimeout(600)
-    googlenet_next_sock.connect((args.googlenet_next_addr, args.googlenet_next_port))
-    print('GoogLenet next node is ready, Connected by', args.googlenet_next_addr)
+    if args.googlenet_block == False:
+        googlenet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        googlenet_next_sock.settimeout(600)
+        googlenet_next_sock.connect((args.googlenet_next_addr, args.googlenet_next_port))
+        print('GoogLenet next node is ready, Connected by', args.googlenet_next_addr)
 
-    googlenet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    googlenet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    googlenet_prev_sock.bind((args.googlenet_prev_addr, args.googlenet_prev_port))
-    googlenet_prev_sock.listen()
-    googlenet_port, googlenet_addr = googlenet_prev_sock.accept()
-    print('GoogLenet prev node is ready, Connected by', googlenet_addr)
+        googlenet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        googlenet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        googlenet_prev_sock.bind((args.googlenet_prev_addr, args.googlenet_prev_port))
+        googlenet_prev_sock.listen()
+        googlenet_port, googlenet_addr = googlenet_prev_sock.accept()
+        print('GoogLenet prev node is ready, Connected by', googlenet_addr)
 
-    mobilenet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mobilenet_next_sock.settimeout(600)
-    mobilenet_next_sock.connect((args.mobilenet_next_addr, args.mobilenet_next_port))
-    print('MobileNet next node is ready, Connected by', args.mobilenet_next_addr)
+    if args.mobilenet_block == False:
+        mobilenet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mobilenet_next_sock.settimeout(600)
+        mobilenet_next_sock.connect((args.mobilenet_next_addr, args.mobilenet_next_port))
+        print('MobileNet next node is ready, Connected by', args.mobilenet_next_addr)
 
-    mobilenet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mobilenet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    mobilenet_prev_sock.bind((args.mobilenet_prev_addr, args.mobilenet_prev_port))
-    mobilenet_prev_sock.listen()
-    mobilenet_port, mobilenet_addr = mobilenet_prev_sock.accept()
-    print('MobileNet prev node is ready, Connected by', mobilenet_addr)
+        mobilenet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mobilenet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        mobilenet_prev_sock.bind((args.mobilenet_prev_addr, args.mobilenet_prev_port))
+        mobilenet_prev_sock.listen()
+        mobilenet_port, mobilenet_addr = mobilenet_prev_sock.accept()
+        print('MobileNet prev node is ready, Connected by', mobilenet_addr)
 
-    vggnet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    vggnet_next_sock.settimeout(600)
-    vggnet_next_sock.connect((args.vggnet_next_addr, args.vggnet_next_port))
-    print('VGGNet next node is ready, Connected by', args.vggnet_next_addr)
+    if args.vggnet_block == False:
+        vggnet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        vggnet_next_sock.settimeout(600)
+        vggnet_next_sock.connect((args.vggnet_next_addr, args.vggnet_next_port))
+        print('VGGNet next node is ready, Connected by', args.vggnet_next_addr)
 
-    vggnet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    vggnet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    vggnet_prev_sock.bind((args.vggnet_prev_addr, args.vggnet_prev_port))
-    vggnet_prev_sock.listen()
-    vggnet_port, vggnet_addr = vggnet_prev_sock.accept()
-    print('VGGNet prev node is ready, Connected by', vggnet_addr)
+        vggnet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        vggnet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        vggnet_prev_sock.bind((args.vggnet_prev_addr, args.vggnet_prev_port))
+        vggnet_prev_sock.listen()
+        vggnet_port, vggnet_addr = vggnet_prev_sock.accept()
+        print('VGGNet prev node is ready, Connected by', vggnet_addr)
 
-    vggfnet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    vggfnet_next_sock.settimeout(600)
-    vggfnet_next_sock.connect((args.vggfnet_next_addr, args.vggfnet_next_port))
-    print('VGGFNet next node is ready, Connected by', args.vggfnet_next_addr)
+    if args.vggfnet_block == False:
+        vggfnet_next_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        vggfnet_next_sock.settimeout(600)
+        vggfnet_next_sock.connect((args.vggfnet_next_addr, args.vggfnet_next_port))
+        print('VGGFNet next node is ready, Connected by', args.vggfnet_next_addr)
 
-    vggfnet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    vggfnet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    vggfnet_prev_sock.bind((args.vggfnet_prev_addr, args.vggfnet_prev_port))
-    vggfnet_prev_sock.listen()
-    vggfnet_port, vggfnet_addr = vggfnet_prev_sock.accept()
-    print('VGGFNet prev node is ready, Connected by', vggfnet_addr)
-    '''
+        vggfnet_prev_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        vggfnet_prev_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        vggfnet_prev_sock.bind((args.vggfnet_prev_addr, args.vggfnet_prev_port))
+        vggfnet_prev_sock.listen()
+        vggfnet_port, vggfnet_addr = vggfnet_prev_sock.accept()
+        print('VGGFNet prev node is ready, Connected by', vggfnet_addr)
 
     input('Enter any key...')
 
     _stop_event = threading.Event()
-
-    alexnet_label_list = []
-    alexnet_time_list = []
-    '''
-    googlenet_label_list = []
-    googlenet_time_list = []
-    mobilenet_label_list = []
-    mobilenet_time_list = []
-    vggnet_label_list = []
-    vggnet_time_list = []
-    vggfnet_label_list = []
-    vggfnet_time_list = []
-    '''
-
-    alexnet_label_lock = threading.Lock()
-    alexnet_time_lock = threading.Lock()
-    '''
-    googlenet_label_lock = threading.Lock()
-    googlenet_time_lock = threading.Lock()
-    mobilenet_label_lock = threading.Lock()
-    mobilenet_time_lock = threading.Lock()
-    vggnet_label_lock = threading.Lock()
-    vggnet_time_lock = threading.Lock()
-    vggfnet_label_lock = threading.Lock()
-    vggfnet_time_lock = threading.Lock()
-    '''
-
     procs = []
-    procs.append(threading.Thread(target=image_sender, args=("alexnet", alexnet_next_sock, images, labels, alexnet_label_list, alexnet_label_lock, alexnet_time_list, alexnet_time_lock, args.alexnet_wait_time, args.alexnet_arrival_rate, _stop_event)))
-    procs.append(threading.Thread(target=image_recver, args=("alexnet", alexnet_port, alexnet_label_list, alexnet_label_lock, alexnet_time_list, alexnet_time_lock, _stop_event)))
-    '''
-    procs.append(threading.Thread(target=image_sender, args=("googlenet", googlenet_next_sock, images, labels, googlenet_label_list, googlenet_label_lock, googlenet_time_list, googlenet_time_lock, args.googlenet_wait_time, args.googlenet_arrival_rate, _stop_event)))
-    procs.append(threading.Thread(target=image_recver, args=("googlenet", googlenet_port, googlenet_label_list, googlenet_label_lock, googlenet_time_list, googlenet_time_lock, _stop_event)))
-    procs.append(threading.Thread(target=image_sender, args=("mobilenet", mobilenet_next_sock, images, labels, mobilenet_label_list, mobilenet_label_lock, mobilenet_time_list, mobilenet_time_lock, args.mobilenet_wait_time, args.mobilenet_arrival_rate, _stop_event)))
-    procs.append(threading.Thread(target=image_recver, args=("mobilenet", mobilenet_port, mobilenet_label_list, mobilenet_label_lock, mobilenet_time_list, mobilenet_time_lock, _stop_event)))
-    procs.append(threading.Thread(target=image_sender, args=("vggnet", vggnet_next_sock, images, labels, vggnet_label_list, vggnet_label_lock, vggnet_time_list, vggnet_time_lock, args.vggnet_wait_time, args.vggnet_arrival_rate, _stop_event)))
-    procs.append(threading.Thread(target=image_recver, args=("vggnet", vggnet_port, vggnet_label_list, vggnet_label_lock, vggnet_time_list, vggnet_time_lock, _stop_event)))
-    procs.append(threading.Thread(target=image_sender, args=("vggfnet", vggfnet_next_sock, images, labels, vggfnet_label_list, vggfnet_label_lock, vggfnet_time_list, vggfnet_time_lock, args.vggfnet_wait_time, args.vggfnet_arrival_rate, _stop_event)))
-    procs.append(threading.Thread(target=image_recver, args=("vggfnet", vggfnet_port, vggfnet_label_list, vggfnet_label_lock, vggfnet_time_list, vggfnet_time_lock, _stop_event)))
-    '''
+
+    if args.alexnet_block == False:
+        alexnet_label_list = []
+        alexnet_time_list = []
+        alexnet_label_lock = threading.Lock()
+        alexnet_time_lock = threading.Lock()
+        procs.append(threading.Thread(target=image_sender, args=("alexnet", alexnet_next_sock, images, labels, alexnet_label_list, alexnet_label_lock, alexnet_time_list, alexnet_time_lock, args.alexnet_arrival_rate, _stop_event)))
+        procs.append(threading.Thread(target=image_recver, args=("alexnet", alexnet_port, alexnet_label_list, alexnet_label_lock, alexnet_time_list, alexnet_time_lock, _stop_event)))
+
+    if args.googlenet_block == False:
+        googlenet_label_list = []
+        googlenet_time_list = []
+        googlenet_label_lock = threading.Lock()
+        googlenet_time_lock = threading.Lock()
+        procs.append(threading.Thread(target=image_sender, args=("googlenet", googlenet_next_sock, images, labels, googlenet_label_list, googlenet_label_lock, googlenet_time_list, googlenet_time_lock, args.googlenet_arrival_rate, _stop_event)))
+        procs.append(threading.Thread(target=image_recver, args=("googlenet", googlenet_port, googlenet_label_list, googlenet_label_lock, googlenet_time_list, googlenet_time_lock, _stop_event)))
+
+    if args.mobilenet_block == False:
+        mobilenet_label_list = []
+        mobilenet_time_list = []
+        mobilenet_label_lock = threading.Lock()
+        mobilenet_time_lock = threading.Lock()
+        procs.append(threading.Thread(target=image_sender, args=("mobilenet", mobilenet_next_sock, images, labels, mobilenet_label_list, mobilenet_label_lock, mobilenet_time_list, mobilenet_time_lock, args.mobilenet_arrival_rate, _stop_event)))
+        procs.append(threading.Thread(target=image_recver, args=("mobilenet", mobilenet_port, mobilenet_label_list, mobilenet_label_lock, mobilenet_time_list, mobilenet_time_lock, _stop_event)))
+
+    if args.vggnet_block == False:
+        vggnet_label_list = []
+        vggnet_time_list = []
+        vggnet_label_lock = threading.Lock()
+        vggnet_time_lock = threading.Lock()
+        procs.append(threading.Thread(target=image_sender, args=("vggnet", vggnet_next_sock, images, labels, vggnet_label_list, vggnet_label_lock, vggnet_time_list, vggnet_time_lock, args.vggnet_arrival_rate, _stop_event)))
+        procs.append(threading.Thread(target=image_recver, args=("vggnet", vggnet_port, vggnet_label_list, vggnet_label_lock, vggnet_time_list, vggnet_time_lock, _stop_event)))
+
+    if args.vggfnet_block == False:
+        vggfnet_label_list = []
+        vggfnet_time_list = []
+        vggfnet_label_lock = threading.Lock()
+        vggfnet_time_lock = threading.Lock()
+        procs.append(threading.Thread(target=image_sender, args=("vggfnet", vggfnet_next_sock, images, labels, vggfnet_label_list, vggfnet_label_lock, vggfnet_time_list, vggfnet_time_lock, args.vggfnet_arrival_rate, _stop_event)))
+        procs.append(threading.Thread(target=image_recver, args=("vggfnet", vggfnet_port, vggfnet_label_list, vggfnet_label_lock, vggfnet_time_list, vggfnet_time_lock, _stop_event)))
 
     for proc in procs:
         proc.start()
@@ -195,15 +200,22 @@ if __name__ == "__main__":
     for proc in procs:
         proc.join()
 
-    alexnet_next_sock.close()
-    alexnet_prev_sock.close()
-    '''
-    googlenet_next_sock.close()
-    googlenet_prev_sock.close()
-    mobilenet_next_sock.close()
-    mobilenet_prev_sock.close()
-    vggnet_next_sock.close()
-    vggnet_prev_sock.close()
-    vggfnet_next_sock.close()
-    vggfnet_prev_sock.close()
-    '''
+    if args.alexnet_block == False:
+        alexnet_next_sock.close()
+        alexnet_prev_sock.close()
+
+    if args.googlenet_block == False:
+        googlenet_next_sock.close()
+        googlenet_prev_sock.close()
+
+    if args.mobilenet_block == False:
+        mobilenet_next_sock.close()
+        mobilenet_prev_sock.close()
+
+    if args.vggnet_block == False:
+        vggnet_next_sock.close()
+        vggnet_prev_sock.close()
+
+    if args.vggfnet_block == False:
+        vggfnet_next_sock.close()
+        vggfnet_prev_sock.close()
