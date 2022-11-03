5e95a51c10061d70270678dc2c9939072bc40aa6
yhjh5302 <yhjh5302@naver.com>
2022-11-03 21:57:37 +0900

    Yolo Docker Nano Test

---
+++ tensorflow-piecewise/YoloV3/pretrained_wights.sh
@@ -0,0 +1,4 @@
+wget https://pjreddie.com/media/files/yolov3.weights -O data/yolov3.weights
+python convert.py --weights ./data/yolov3.weights --output ./checkpoints/yolov3.tf
+wget https://pjreddie.com/media/files/yolov3-tiny.weights -O data/yolov3-tiny.weights
+python convert.py --weights ./data/yolov3-tiny.weights --output ./checkpoints/yolov3-tiny.tf --tiny
