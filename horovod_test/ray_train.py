4a172d75cf184a47e619dea8bb9e2fd7e62f58cc
User <yhjh5302@gmail.com>
2023-01-17 16:24:43 +0900

    horovod_update

--- horovod_test/ray_train.py
+++ horovod_test/ray_train.py
@@ -1,6 +1,9 @@
 from models import *
+from typing import Dict
+import argparse, ray
 
-if __name__ == '__main__':
+
+def train_func(config: Dict):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
     print(torch.cuda.get_device_name(0))
@@ -10,25 +13,26 @@ if __name__ == '__main__':
     train_dataset = torchvision.datasets.CIFAR10(root='./cifar10_data', train=True, download=True, transform=transform)
 
     # Partition dataset among workers using DistributedSampler
-    batch_size = 64
-    num_workers = 1
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
+    batch_size = config["batch_size"]
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
+    train_loader = ray.train.torch.prepare_data_loader(train_loader) # ray train data loader
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     
     # Build model
     model = AlexNet()
     model = model.cuda()
+    model = ray.train.torch.prepare_model(model)
     #from torchsummary import summary
     #summary(model, (3, 224, 224))
 
     import torch.optim as optim
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=config["lr"], momentum=config["momentum"])
 
-    epoch_size = 10
-    verbose = 1
+    epoch_size = config["epoch_size"]
+    verbose = config["verbose"]
     for epoch in range(epoch_size):   # repeat process with same data
-        running_loss = 0.0
+        running_loss, epoch_loss = 0.0, 0.0
         for i, data in enumerate(train_loader, 0):
             # receive inputs from data
             inputs, labels = data[0].cuda(), data[1].cuda()
@@ -44,13 +48,29 @@ if __name__ == '__main__':
 
             # print progress
             running_loss += loss.item()
+            epoch_loss += loss.item()
             if i % verbose == verbose - 1:
                 print('[%2d/%2d,%4d/%4d] loss: %.3f' % (epoch + 1, epoch_size, i + 1, len(train_dataset)/batch_size, running_loss / verbose))
                 running_loss = 0.0
-
     print('Finished Training')
 
     #### save trained model
     # PATH = './'
     # torch.save(model.state_dict(), PATH + 'cifar_AlexNet.pth')
     # print('Model Saved')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N', help='number of data loading workers (default: 4)')
+    parser.add_argument('--master_addr', default='localhost', type=str, help='Master node ip address')
+    parser.add_argument('--master_port', default='30000', type=str, help='Master node port')
+    parser.add_argument('--ifname', default='ens3f0', type=str, help='Master node port')
+    parser.add_argument('--backend', default='nccl', choices=['gloo', 'mpi', 'nccl'], type=str, help='distributed backend')
+    args = parser.parse_args()
+
+    trainer = ray.train.torch.TorchTrainer(
+        train_loop_per_worker=train_func,
+        train_loop_config={"lr": 1e-3, "momentum": 0.9, "batch_size": 64, "epoch_size": 10, "verbose": 1},
+        scaling_config=ray.air.config.ScalingConfig(num_workers=args.workers, use_gpu=True),
+    )
+    trainer.fit()
